f56aec9f7a532bb3d57c9e3637996fb6c995faba
Henry <lkhh@protonmail.com>
2021-05-07 18:14:13 +0800

    Fix event print format

--- src/bot/printEvent.ts
+++ src/bot/printEvent.ts
@@ -2,7 +2,10 @@ const printEvent = (desc: string, responses: any[], date: Date) => {
   return `${desc}
 ${date}
 
-${responses.map((res, idx) => `${idx + 1}. ${res.nickname}${res.confirmed ? '' : ' (TBC)'}`)}`
+${responses
+  .map((res, idx) => `${idx + 1}. ${res.nickname}${res.confirmed ? '' : ' (tbc)'}`)
+  .toString()
+  .replace(/,/g, '\n')}`
 }
 
 export default printEvent
