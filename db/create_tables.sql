667190f69138c5017ab56ae897d0ae03be11336e
Henry <lkhh@protonmail.com>
2021-05-07 16:32:06 +0800

    Change responses table structure

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -19,12 +19,12 @@ CREATE TABLE public.users (
 );
 
 CREATE TABLE public.responses (
-    resp_id uuid NOT NULL primary key DEFAULT uuid_generate_v4(),
     event_id uuid NOT NULL,
     user_id integer NOT NULL,
     nickname character varying(255),
     confirmed boolean DEFAULT true NOT NULL,
     append integer DEFAULT 0,
+    PRIMARY KEY(event_id, user_id),
   	FOREIGN key (event_id) REFERENCES events(event_id) on delete CASCADE,
   	FOREIGN key (user_id) REFERENCES users(user_id) on delete CASCADE
 );
