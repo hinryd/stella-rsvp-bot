90f3c86eab86f082d2aa8fefd153fc561b042d54
Henry <lkhh@protonmail.com>
2021-05-07 17:08:44 +0800

    Release alpha 0.1.0, all basic commands implemented

--- src/bot/session.ts
+++ src/bot/session.ts
@@ -2,6 +2,7 @@ import RedisSession from 'telegraf-session-redis'
 
 const session = new RedisSession({
   store: {
+    url: process.env.REDIS_URL,
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
   },
