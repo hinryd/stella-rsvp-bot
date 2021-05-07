d00bc8208649215b293b5291d9d4a2b820ae12ac
Henry <lkhh@protonmail.com>
2021-05-07 20:17:39 +0800

    Update event print format

--- src/bot/printEvent.ts
+++ src/bot/printEvent.ts
@@ -1,6 +1,8 @@
-const printEvent = (desc: string, responses: any[], date: Date) => {
+import { DateTime } from 'luxon'
+
+const printEvent = (desc: string, responses: any[], date: string) => {
   return `${desc}
-${date}
+Date: ${DateTime.fromISO(date).toFormat('yyyy-LLL-d (ccc) HH:mm')}
 
 ${responses
   .map((res, idx) => `${idx + 1}. ${res.nickname}${res.confirmed ? '' : ' (tbc)'}`)
