666fb5cdb84a367d39c1739679e4e0b5ee1d1112
Henry <lkhh@protonmail.com>
2021-05-18 17:42:44 +0800

    Add type annotation

--- src/commands/del.ts
+++ src/commands/del.ts
@@ -1,9 +1,8 @@
 import BotCtx from '../bot/BotCtx.interface'
 import { Markup } from 'telegraf'
 import supabase from '../utils/supabase'
-import printEvent from '../bot/printEvent'
 
-const del = async (ctx: BotCtx) => {
+const del = async (ctx: BotCtx): Promise<void> => {
   const { data, error } = await supabase
     .from('events')
     .select('event_id, event_desc, event_date, updated_at')
@@ -15,10 +14,10 @@ const del = async (ctx: BotCtx) => {
     ctx.reply(
       'Select an event to delete',
       Markup.inlineKeyboard(
-        data.map(event => {
+        data.map((event) => {
           return [Markup.button.callback(event.event_desc, `del|${event.event_id}`)]
-        })
-      )
+        }),
+      ),
     )
   }
 }
