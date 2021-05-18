0b7b1b0cad5539e881ee34b96d911689c12211e1
Henry <lkhh@protonmail.com>
2021-05-18 19:24:29 +0800

    Overhaul event creator

--- src/scenes/eventCreator.ts
+++ src/scenes/eventCreator.ts
@@ -6,6 +6,7 @@ import printEvent from '../bot/printEvent'
 
 export interface EventCreatorSession extends Scenes.WizardSession {
   state: {
+    messages: any[]
     event_desc: string
     event_date: any
   }
@@ -14,27 +15,33 @@ export interface EventCreatorSession extends Scenes.WizardSession {
 const eventCreator = new Scenes.WizardScene<BotCtx>(
   'EVENT_CREATOR',
   async (ctx) => {
-    ctx.session.state = { event_desc: '', event_date: new Date() }
-    await ctx
+    ctx.session.state.event_desc = ''
+    ctx.session.state.event_date = new Date()
+    ctx.session.state.messages = []
+    const message = await ctx
       .reply(
         '[1/2] Reply this message to set your event description',
-        Markup.forceReply().selective(),
+        Markup.forceReply().selective()
       )
-      .catch((err) => console.log(err))
+      .catch((err) => console.error('CREATE-SCENE-1', err))
+    ctx.session.state.messages.push(message)
     return ctx.wizard.next()
   },
+
   async (ctx) => {
     if (ctx.message === undefined || !('text' in ctx.message))
       throw new Error('Missing ctx.message')
     ctx.session.state.event_desc = ctx.message.text
-    await ctx
+    const message = await ctx
       .reply(
         `[2/2] Reply this message to set your event date and time, e.g. 'tomorrow 2 pm' or 'june 26 17:00'`,
-        Markup.forceReply().selective(),
+        Markup.forceReply().selective()
       )
-      .catch((err) => console.log(err))
+      .catch((err) => console.error('CREATE-SCENE-2', err))
+    ctx.session.state.messages.push(message)
     return ctx.wizard.next()
   },
+
   async (ctx) => {
     if (ctx.message === undefined || !('text' in ctx.message))
       throw new Error('Missing ctx.message')
@@ -42,36 +49,38 @@ const eventCreator = new Scenes.WizardScene<BotCtx>(
     const eventDate = Date.create(ctx.message.text, { fromUTC: true })
     ctx.session.state.event_date = eventDate ? eventDate : new Date()
 
-    const { error } = await supabase.from('groups').upsert({ group_id: ctx.message.chat.id })
-
-    if (!error) {
-      await supabase
-        .from('events')
-        .upsert({
+    await supabase
+      .from('groups')
+      .upsert({ group_id: ctx.message.chat.id })
+      .then(async (res) => {
+        const { data, error } = await supabase.from('events').upsert({
           group_id: ctx.message?.chat.id,
           event_desc: ctx.session.state.event_desc,
-          event_date: ctx.session.state.event_date,
+          event_date: ctx.session.state.event_date
         })
-        .then(
-          async (res: { data: any }) =>
-            await ctx
-              .reply(
-                printEvent(res.data[0].event_desc, [], res.data[0].event_date),
-                Markup.inlineKeyboard([
-                  Markup.button.callback('yes', `yes|${res.data[0].event_id}`),
-                  Markup.button.callback('maybe', `maybe|${res.data[0].event_id}`),
-                  Markup.button.callback('no', `no|${res.data[0].event_id}`),
-                  // Markup.button.callback('+1', `addOne:${event.event_id}`),
-                ]),
-              )
-              .catch((err) => console.log(err)),
-        )
-    } else {
-      await ctx.reply(JSON.stringify(error))
-    }
+        if (!error && data !== null) {
+          await ctx
+            .reply(
+              printEvent(data[0].event_desc, [], data[0].event_date),
+              Markup.inlineKeyboard([
+                Markup.button.callback('yes', `yes|${data[0].event_id}`),
+                Markup.button.callback('maybe', `maybe|${data[0].event_id}`),
+                Markup.button.callback('no', `no|${data[0].event_id}`)
+                // Markup.button.callback('+1', `addOne:${event.event_id}`),
+              ])
+            )
+            .catch((err) => console.error('CREATE-SCENE-3', err))
+        } else {
+          console.log('CREATE-SCENE-SUPABASE-2')
+        }
+      })
 
-    return await ctx.scene.leave()
-  },
+    ctx.session.state.messages.map(async ({ message_id }) => {
+      await ctx.deleteMessage(message_id).catch((err) => console.log('CREATE-SCENE-DELETE', err))
+    })
+
+    return ctx.scene.leave()
+  }
 )
 
 export default eventCreator
